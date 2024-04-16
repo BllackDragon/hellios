b5f47edfa301cfe1cb1ef5c3ee2dccd345ea2046
BllackDragon <andreytyutyunnik2004@gmail.com>
2024-04-16 18:48:02 +0700

    первая версия проекта

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ru">
+<html>
+    <title> Советские светосильные объективы Helios-44 </title>
+    <head>
+        <style>
+            body{
+                background-color: #f8cb91;
+            }
+            .nolist{
+                list-style-type: none;
+                padding-left: 0;
+            }
+            .pb{
+             font-weight: bold;   
+            }
+            
+        </style>
+        <h1 align="center">Гелиос-44: разнообразие советской оптики</h1>
+           <center><img width="600" height="400" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/%D0%93%D0%B5%D0%BB%D0%B8%D0%BE%D1%81%D1%8B.jpg/1200px-%D0%93%D0%B5%D0%BB%D0%B8%D0%BE%D1%81%D1%8B.jpg" alt="Фото разных моделей Helios"></center>
+        <h2>Существует множество моделей данных объективов, отличающихся сборкой и компоновкой.</h2>
+        <p class="pb" align="center">В основном, они отличаются такими вещами:</p>
+        <ul>
+            <li>заводом изготовителем</li>
+            <li>посадочной резьбой или байонетом</li>
+            <li>оправой корпуса</li>
+            <li>наличием репетира диафрагмы</li>
+            <li>наличием кольца предустановки диафрагмы</li>
+            <li>наличием мультипросветления (маркировка 'МС')</li>
+            <li>именами, написанными на латыни или кириллице</li>
+            <li>И множеством других мелочей</li>
+            </ul>
+
+        <p class="pb" align="center">Объективы ГЕЛИОС-44 выпускались на нескольких заводах:</p>
+<ol>
+<li>КМЗ - Красногорский Механический Завод</li>
+<li>ММЗ - Минский Механический Завод имени С. И. Вавилова, с 1971 ставший БелОМО (Белорусское Оптико-Механическое Объединение)</li>
+<li>Оптико-механический завод "Юпитер", Валдай</li>
+<li><a href="https://www.zenit.photo/?ysclid=lo9oos7l5p14091939" target="blank">Завод ‘Зенит’</a>,Вилейка</li>
+</ol>
+<center><h2>Завод 'Валдай'</h2></center>
+<p class="pb">На заводе Валдай объективы выпускались сразу и только под резьбу M42х1/45,5. На этом заводе Гелиосы начали выпускать приблизительно с 1969.</p>
+<table align="center">
+<tr>
+<td><p class="pb" align="center">HELIOS-44-2 2/58 h301</p></td>
+<td><p class="pb" align="center">Helios-44М 2/58 h301-a1</p></td>
+<td><p class="pb" align="center">ZENIT MC HELIOS-44М-4 58mm 1:2 h306</p></td>
+</tr>
+<tr>
+<td><center><img width="200" height="200" src="il_570xN.1684955323_o5wh.jpg" alt="HELIOS-44-2 2/58 h301"></center></td>
+<td><center><img width="250" height="200" src="helios-44-m-301a-valdai-5.jpg" alt="helios-44-m-301a-valdai-5"></center></td>
+<td><center><img width="300" height="200" src="2ba679b3a01911e69be360eb69e1549c_11-900x600.jpg" alt="HELIOS-44М-4"></center></td>
+</tr>
+<td>
+    <ul class="nolist">
+        <li>Тип: Постоянное фокусное расстояние (фикс)</li>
+        <li>Фокус: Ручная фокусировка</li>
+        <li>Система: M42</li>
+        <li>Год начала производства: 1950</li>
+        <li>Год окончания производства: 1990</li>
+        <li>Оптическая схема: 6 элементов в 4 группах</li>
+    </ul>    
+</td>
+<td>
+    <ul class="nolist">
+        <li>Тип: Постоянное фокусное расстояние (фикс)</li>
+        <li>Фокус: Ручная фокусировка</li>
+        <li>Система: M42</li>
+        <li>Год начала производства: 1950</li>
+        <li>Год окончания производства: 1990</li>
+        <li>Оптическая схема: 6 элементов в 4 группах</li>
+        <li>Диафрагма: моргающая</li>
+    </ul>     
+</td>
+<td>
+    <ul class="nolist">
+        <li>Тип: Постоянное фокусное расстояние (фикс)</li>
+        <li>Фокус: Ручная фокусировка</li>
+        <li>Система: M42</li>
+        <li>Оптическая схема: 6 элементов в 4 группах</li>
+    </ul>     
+</td>
+</table>
+
+<div><form>
+    <p class="pb">  
+        <br><label for="product">Снимали ли вы когда либо на объективы Helios?</label>
+        <select name="product" id="product">
+        <option value="1">Да</option>
+        <option value="2">Нет</option>
+        <option value="3">Затрудняюсь ответить</option>
+        <input type="submit" value="Отпарвить">
+        </select><br>
+    </form></div>
+
+<div><form>
+    <p class="pb">
+     <label for="qestions">Если да, то расскажите с помощью какого объектива Вы делали снимки и Ваши впечатления:</label>
+     <textarea name="qestions" id="qestions" required></textarea>
+    </form></div>
+</p>
+    </head>
+</html>
